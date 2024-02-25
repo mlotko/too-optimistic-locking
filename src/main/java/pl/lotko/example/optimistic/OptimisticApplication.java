a271f89a3570cbc14e5ea4b9bf5b02f3f1e9cdc4
Mariusz Lotko <mariusz.lotko@gmail.com>
2024-02-25 20:24:36 +0100

    Spring generated

---
+++ src/main/java/pl/lotko/example/optimistic/OptimisticApplication.java
@@ -0,0 +1,13 @@
+package pl.lotko.example.optimistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimisticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimisticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lotko/example/optimistic/OptimisticApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lotko.example.optimistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimisticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
