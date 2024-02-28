538b2af0ffac2c1a7c75cc0d8370e3177695ecc2
Mariusz Lotko <mariusz.lotko@gmail.com>
2024-02-28 01:05:54 +0100

    Pessimistic attempt

--- src/main/java/pl/lotko/example/optimistic/phonebook/persistence/PhoneBookItemRepository.java
+++ src/main/java/pl/lotko/example/optimistic/phonebook/persistence/PhoneBookItemRepository.java
@@ -1,6 +1,13 @@
 package pl.lotko.example.optimistic.phonebook.persistence;
 
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface PhoneBookItemRepository extends CrudRepository<PhoneBookItemEntity, Long> {
+    @Override
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<PhoneBookItemEntity> findById(Long aLong);
 }
